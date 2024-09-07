ed4155f878943aa2f0314889c713ba4ec152db11
Atif <iamatif96@gmail.com>
2024-09-07 23:02:54 +0800

    producer with callback

--- kafka-basics/src/main/java/org/atif/com/demos/produce/ProducerDemoWithCallBack.java
+++ kafka-basics/src/main/java/org/atif/com/demos/produce/ProducerDemoWithCallBack.java
@@ -1,9 +1,6 @@
 package org.atif.com.demos.produce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,32 +23,51 @@ public class ProducerDemoWithCallBack {
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
 
+        //DO NOT DO this in PRODUCTION
+        //just for understanding partitions
+
+        //setting batch size to 400, so we can change partitioner after size finish
+        //but by default it is 16KB
+//        properties.setProperty("batch.size", "400");
+
+        //setting partitioner to round-robin
+//        properties.setProperty("partitioner.class", RoundRobinPartitioner.class.getName());
+
+
         //create producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        //we want to produce kafka messages 10 times
-        for (int i = 0; i < 10; i++) {
-            //create producer record
-            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "Hello there " + i);
+        //
+        for (int j = 0; j < 10; j++) {
+            //we want to produce kafka messages for batch of 30
+            for (int i = 0; i < 30; i++) {
+                //create producer record
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "Hello there " + i);
 
-            //send data
-            producer.send(producerRecord, new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata metadata, Exception exception) {
-                    //execute everytime when a record successfully sent an exception is thrown
-                    if (exception != null) {
-                        log.error("Error while producing", exception);
-                    } else {
-                        //the record sent successfully
-                        log.info("Received new metadata \n" +
-                                "Topic: " + metadata.topic() + "\n" +
-                                "Partition: " + metadata.partition() + "\n" +
-                                "Offset: " + metadata.offset() + "\n" +
-                                "Timestamp: " + metadata.timestamp()
-                        );
+                //send data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+                        //execute everytime when a record successfully sent an exception is thrown
+                        if (exception != null) {
+                            log.error("Error while producing", exception);
+                        } else {
+                            //the record sent successfully
+                            log.info("Received new metadata \n" +
+                                    "Topic: " + metadata.topic() + "\n" +
+                                    "Partition: " + metadata.partition() + "\n" +
+                                    "Offset: " + metadata.offset() + "\n" +
+                                    "Timestamp: " + metadata.timestamp()
+                            );
+                        }
                     }
-                }
-            }); //.send() - it runs asynchronously unless we use flush or close.
+                }); //.send() - it runs asynchronously unless we use flush or close.
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         //tell the producer to send all data and block until done --synchronous
